c66e1c16844a4113ca636cf5190be86b5c1ac487
Golden.Gao <gsixin@live.com>
2025-02-23 23:16:08 +0800

    MR-11 refactor (#7)

---
+++ MR/MR.Service/ExtensionUtilities/RandomExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MR.Service.ExtensionUtilities
+{
+    public class RandomExtension
+    {
+        public static Random GetRandom()
+        {
+            long tick = DateTime.Now.Ticks;
+            var rand = new Random((int)(tick & 0xffffffffL) | (int)(tick >> 32));
+            return rand;
+        }
+    }
+}
